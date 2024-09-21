6c007c201de37dbb0e7b886694964c92aef028f9
ncwjsp <nc.wjsp@gmail.com>
2024-09-22 02:54:35 +0700

    add upload api

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
+import { withUt } from "uploadthing/tw";
 
-const config: Config = {
+const config: Config = withUt({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{ts,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -15,5 +17,5 @@ const config: Config = {
     },
   },
   plugins: [require("daisyui")],
-};
+});
 export default config;
